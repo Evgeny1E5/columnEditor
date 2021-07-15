fe100fa41d43da76ec87a12c484000cf9f26f6e9
Evgeny Sinokop <kenda.prg@gmail.com>
2021-07-15 21:56:10 +0300

    ver 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.rmi.activation.ActivationGroupDesc;
 import java.util.*;
 
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
@@ -11,7 +12,7 @@ public class Main {
     static ArrayList<String> list = new ArrayList<>();
     static BufferedWriter bwFile;
     static BufferedReader brFile;
-    static String fileToRead = "test.txt";
+    static String fileToRead = "blockchair_bitcoin_addresses_latest.tsv";
 
     public static void main(String[] args) throws Exception {
         /*String array[] = "hello".split("");
@@ -20,6 +21,7 @@ public class Main {
         array[3] = oneItem;
         System.out.println(String.join("", array));*/
        // System.out.println("Enter file name");
+        Test.test();
         System.out.println(Runtime.getRuntime().totalMemory());
         System.out.println(Runtime.getRuntime().maxMemory());
         System.out.println(Runtime.getRuntime().availableProcessors());
@@ -34,7 +36,7 @@ public class Main {
             //file to read
             brFile = new BufferedReader(new FileReader(fileToRead));
 
-            System.out.println("Wait for command... move or del");
+            System.out.println("Wait for command... move, del, uniq, split, merge");
             lineCommand = brConsole.readLine();
             arrSplited = lineCommand.split(" ");
             System.out.println("Reading file...");
@@ -47,13 +49,28 @@ public class Main {
             if(arrSplited[0].equals("move")){
                 move(Integer.valueOf(arrSplited[1])-1, Integer.valueOf(arrSplited[2])-1);
                 System.out.println("Command " + lineCommand + " ended");
-            }else if(arrSplited[0].equals("del")){
+            }
+            if(arrSplited[0].equals("del")){
                 if(arrSplited.length == 2){
                     del(Integer.valueOf(arrSplited[1])-1);
+                    System.out.println("Command " + lineCommand + " ended");
                 } else del(Integer.valueOf(arrSplited[1])-1, Integer.valueOf(arrSplited[2])-1);
-            }else if(arrSplited[0].equals("uniq")){
+            }
+            if(arrSplited[0].equals("uniq")){
                 System.out.println("Execute unique");
                 unique();
+            }
+            if(arrSplited[0].equals("split")){
+                split();
+                System.out.println("Command " + lineCommand + " ended");
+            }
+            if(arrSplited[0].equals("merge")){
+                mergeAddresses();
+                System.out.println("Command " + lineCommand + " ended");
+            }
+            if(arrSplited[0].equals("parse")){
+                parsePublicKeyFromBlockchair();
+                System.out.println("Command " + lineCommand + " ended");
             }
             brFile.close();
             list.clear();
@@ -156,8 +173,91 @@ public class Main {
         hashSet.clear();
         brFile.close();
         bwFile.close();
+    }
 
+    static void mergeAddresses() throws Exception {
+        String lineFile, lineFileOut, line;
+        boolean firstNewLine = false;
+        HashMap<String, Long> listBalances = new HashMap<>();
+
+        bwFile = new BufferedWriter(new FileWriter("out.txt"));
+        BufferedReader brFile2 = new BufferedReader(new FileReader("blockchair_bitcoin-sv_addresses_latest.tsv"));
+        BufferedReader brFile3 = new BufferedReader(new FileReader("blockchair_bitcoin-cash_addresses_latest.tsv"));
+
+
+
+        while ((lineFile = brFile.readLine()) != null) {
+            String arr[] = lineFile.split("\t");
+            listBalances.put(arr[0], Long.valueOf(arr[1]));
+            if (listBalances.size() % 1000000 == 0) {
+                System.out.println(listBalances.size());
+            }
+        }
+        System.out.println("Finished first loop");
+
+        //for merge BTC, BSV
+        while((line = brFile2.readLine()) != null) {
+            String arr[] = line.split("\t");
+
+                //  System.out.println(arr[0]);
+            if (listBalances.size() % 1000000 == 0) {
+                System.out.println(listBalances.size());
+            }
+                    if (listBalances.get(arr[0]) == null) {
+                        listBalances.put(arr[0], Long.valueOf(arr[1]));
+                    }
+                }
+        System.out.println("Finished second loop");
+
+        //for merge BCH
+        while((line = brFile3.readLine()) != null) {
+            String arr[] = line.split("\t");
+            if (listBalances.size() % 1000000 == 0) {
+                System.out.println(listBalances.size());
+            }
+            if(arr[0].split("")[0].equals("q") || arr[0].split("")[0].equals("p")) {
+                //  System.out.println(arr[0]);
+                if (arr[0].length() > 25) {
+                    if (listBalances.get(com.github.kiulian.converter.AddressConverter.toLegacyAddress(arr[0])) == null) {
+                        listBalances.put(com.github.kiulian.converter.AddressConverter.toLegacyAddress(arr[0]), Long.valueOf(arr[1]));
+                    }
+                }
+            }
+        }
+
+
+
+        for(Map.Entry<String, Long> entry : listBalances.entrySet()) {
+
+            bwFile.write(entry.getKey() + "\t" + entry.getValue());
+            bwFile.newLine();
+        }
+        brFile2.close();
+        brFile3.close();
+        brFile.close();
+        bwFile.close();
+    }
+
+    static void split() throws Exception {
+        String[] stringArray;
+        String oneItem, lineFile;
+        bwFile = new BufferedWriter(new FileWriter("out7.txt"));
+        BufferedWriter bwFile2 = new BufferedWriter(new FileWriter("out8.txt"));
+        while((lineFile = brFile.readLine()) != null){
+            if(lineFile.length() == 31){
+                bwFile.write(lineFile +"\n");
+            }else if(lineFile.length() == 30){
+                bwFile2.write(lineFile +"\n");
+            }
+
+        }
+        bwFile.close();
+        bwFile2.close();
+        brFile.close();
+    }
+    static void parsePublicKeyFromBlockchair(){
 
     }
+
 
 }
---
+++ src/com/company/Test.java
@@ -0,0 +1,11 @@
+package com.company;
+
+
+
+
+public class Test {
+    public static void test() throws Exception{
+        System.out.println(com.github.kiulian.converter.AddressConverter.toLegacyAddress("qr95sy3j9xwd2ap32xkykttr4cvcu7as4y0qverfuy"));
+    }
+
+}
